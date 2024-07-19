0a1fb4d21761fcc8272d28f44b151bbd1a5e999c
Joaquin <joaquintonizzo1@gmail.com>
2024-07-19 12:06:54 -0300

    Estilos ItemDetail

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -4,20 +4,21 @@ import ItemCount from './../ItemCount/ItemCount';
 
 const ItemDetail = ({ name, price, img, stock }) => {
     return (
-        <article className="CardItem">
-            <header className="ItemHeader">
-                <h2>{name}</h2>
-            </header>
-            <img src={img} alt={name} className='ItemImg'/>
-            <section>
-                <p className="Info">Categoría: {stock}</p>
-                <p className="Info">Descripción: {stock}</p>
-                <p className="Info">Precio: ${price}</p>
-                <ItemCount stock={stock} initial={1} />
-            </section>
-            <footer className='ItemFooter'>
-                <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log('Cantidad agregada ', quantity)}/>
-            </footer>
+        <article className="ItemDetail">
+            <img src={img} alt={name} className='DetailImg'/>
+            <div className='DetailInfo'>
+                <header className="DetailHeader">
+                    <h2>{name}</h2>
+                </header>
+                <section>
+                    <p className="DetailCategory">Categoría: {stock}</p>
+                    <p className="DetailDescription">Descripción: {stock}</p>
+                    <p className="DetailPrice">Precio: ${price}</p>
+                </section>
+                <footer className='DetailFooter'>
+                    <ItemCount initial={1} stock={stock} onAdd={(quantity) => console.log('Cantidad agregada ', quantity)}/>
+                </footer>
+            </div>
         </article>
     );
 }
