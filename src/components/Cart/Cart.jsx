8098b6a3ec12941eb6350fa785352f15d9be32f3
Joaquin Tonizzo <joaquintonizzo1@gmail.com>
2024-08-05 21:01:18 -0300

    Ffix total in cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 import CartItem from '../CartItem/CartItem'
 
 const Cart = () => {
-    const { cart, clearCart, removeItem } = useContext(CartContext)
+    const { cart, clearCart, removeItem, total } = useContext(CartContext)
 
     if (cart.length === 0) {
         return (
@@ -19,6 +19,7 @@ const Cart = () => {
     return (
         <div className='CartContainer'>
             {cart.map(p => <CartItem key={p.id} {...p} removeItem={removeItem} />)}
+            <p className='Total'>Total a pagar: {total.toFixed(2)}</p>
             <div>
                 <button onClick={() => clearCart()} className='Button ButtonClear'>Limpiar carrito</button>
                 <Link to='/checkout' className='Button ButtonCheckout'>Checkout</Link>
