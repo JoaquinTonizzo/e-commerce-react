bc934ac1a90c70700654a41630238610fecd5048
Joaquin Tonizzo <joaquintonizzo1@gmail.com>
2024-08-08 12:38:32 -0300

    fix styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import './Footer.css';
+import { FaInstagram, FaFacebook, FaWhatsapp, FaTelegram, FaArrowAltCircleUp } from "react-icons/fa";
+import './Footer.css'; 
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer-content">
-        <p>Desarrollado por Joaquin Gabriel Tonizzo.</p>
-        <p>&reg; Todos los derechos reservados.</p>
-        <p>&copy; 2024 Online Store.</p>
+    <footer className='footer'>
+      <div className="redes">
+        <a href="https://www.instagram.com" target="_blank">
+          <FaInstagram />
+        </a>
+        <a href="https://www.facebook.com" target="_blank">
+          <FaFacebook />        
+        </a>
+        <a href="https://www.whatsapp.com" target="_blank">
+          <FaWhatsapp />  
+        </a>
+        <a href="https://www.telegram.com" target="_blank">
+          <FaTelegram />  
+        </a>
       </div>
+      <a href="/"><p>&reg; Online Store</p></a>
     </footer>
   );
 };
