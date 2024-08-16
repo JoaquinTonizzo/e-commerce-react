5a006d1242848c53748adba82994f9fd2e0ef97d
Joaquin Tonizzo <joaquintonizzo1@gmail.com>
2024-08-16 12:23:07 -0300

    fix in stock after checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -16,7 +16,7 @@ const Checkout = () => {
 
     const createOrder = async ({ name, phone, email }) => {
         setLoading(true);
-
+    
         try {
             const objOrder = {
                 buyer: {
@@ -26,49 +26,50 @@ const Checkout = () => {
                 total: total,
                 date: Timestamp.fromDate(new Date())
             };
-
+    
             const batch = writeBatch(db);
             const outOfStock = [];
             const ids = cart.map(prod => prod.id);
-            const productsRef = collection(db, "products");
-            const productsAddedFromFirestore = await getDocs(query(productsRef, where("id", "in", ids)));
-
-            const { docs } = productsAddedFromFirestore
-
-            docs.forEach(doc => {
+            const productsRef = collection(db, "items");
+            const productsSnapshot = await getDocs(productsRef);
+    
+            const productsInStock = productsSnapshot.docs.filter(doc => 
+                ids.includes(doc.id)
+            );
+    
+            productsInStock.forEach(doc => {
                 const dataDoc = doc.data();
                 const stockDb = dataDoc.stock;
-
+    
                 const productAddedToCart = cart.find(prod => prod.id === doc.id);
                 const prodQuantity = productAddedToCart?.quantity;
-
-                if (stockDb > prodQuantity) {
+    
+                if (stockDb >= prodQuantity) {
                     batch.update(doc.ref, { stock: stockDb - prodQuantity });
                 } else {
                     outOfStock.push({ id: doc.id, ...dataDoc });
                 }
-            })
-
-
+            });
+    
             if (outOfStock.length === 0) {
                 await batch.commit();
-
+    
                 const orderRef = collection(db, "orders");
                 const orderAdded = await addDoc(orderRef, objOrder);
-
+    
                 setOrderId(orderAdded.id);
                 clearCart();
             } else {
                 console.error("Hay productos que están fuera de stock");
             }
-
-
+    
         } catch (error) {
             console.error(error);
         } finally {
             setLoading(false);
         }
-    }
+    };
+    
 
     if (loading) {
         return (
